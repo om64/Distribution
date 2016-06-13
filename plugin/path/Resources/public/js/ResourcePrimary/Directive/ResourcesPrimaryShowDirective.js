635392daffd3e38eba689a1ff42ec83bcce9b824
Elorfin <penin.axel@gmail.com>
2016-06-13 08:14:04 +0200

    improve iFrame resize

--- plugin/path/Resources/public/js/ResourcePrimary/Directive/ResourcesPrimaryShowDirective.js
+++ plugin/path/Resources/public/js/ResourcePrimary/Directive/ResourcesPrimaryShowDirective.js
@@ -18,39 +18,63 @@
                 },
                 bindToController: true,
                 link: function (scope, element, attr) {
-                    var iframeChangeTimeout = null;
+                    $(window).on('message',function(e) {
+                        console.log('received');
+                        console.log(e);
+
+                        if (  (typeof e.originalEvent.data === 'string') && (e.originalEvent.data.indexOf('documentHeight:') > -1) ) {
+
+                            // Split string from identifier
+                            var height = e.originalEvent.data.split('documentHeight:')[1];
+
+                            // do stuff with the height
+                            $(element).css('height', parseInt(height) + 15);
+                        }
+
+                        // Check that message being passed is the documentHeight
+                        /*if (typeof e.data === 'object' && e.data.documentHeight) {
+
+                        }*/
+                    });
+
+                    /*var iframeChangeTimeout = null;
 
                     var resizeIframe = function (element) {
-                        var height = element.find('body').first().height();
+                        var newheight = element.contentDocument.body.scrollHeight;
+
+                        element.height = (newheight) + "px";
+
+                        console.log(newheight);
+
+                        var height = $(element).find('body').first().height();
+
+                        /!*console.log(height);
 
                         if (height) {
-                            element.css('height', height + 15);
-                        }
+                            $(element).css('height', height + 15);
+                        }*!/
                     };
 
                     // Manage the height of the iFrame
                     element.on('load', function () {
-                        var iframe = this;
-                        setTimeout(function () {
-                            resizeIframe(iframe);
-                        }, 50);
+                        console.log('load iframe')
+                        resizeIframe(element);
+
+                        $(element.contentDocument.body).on('resize', function () {
+                            resizeIframe(element)
+                        });
                     });
 
                     $(window).on('resize', function () {
-                        clearTimeout(iframeChangeTimeout);
-                        iframeChangeTimeout = setTimeout(function () {
-                            element.each(function (el) {
-                                resizeIframe(el);
-                            });
-                        }, 300);
-                    });
+                        resizeIframe(element)
+                    });*/
 
-                    clearTimeout(iframeChangeTimeout);
+
+
+                    /*clearTimeout(iframeChangeTimeout);
                     iframeChangeTimeout = setTimeout(function () {
-                        element.each(function () {
-                            resizeIframe(element);
-                        });
-                    }, 300);
+                        resizeIframe(element)
+                    }, 300);*/
                 }
             };
         }
